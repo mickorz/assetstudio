f2a9e0fb39b15400d4dfcc97c8f7860c2ce25036
公司电脑 <490113806@qq.com>
2026-05-19 15:13:04 +0800

    Enable parse logging by default

--- AssetStudio.GUI/Properties/Settings.Designer.cs
+++ AssetStudio.GUI/Properties/Settings.Designer.cs
@@ -520,7 +520,7 @@ namespace AssetStudio.GUI.Properties {
 
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
+        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool enableParseLog {
             get {
                 return ((bool)(this["enableParseLog"]));
